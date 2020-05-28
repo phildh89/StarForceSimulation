11dae1a6754ebb1cd680e1440a056db7b16c0b80
Philip Ho <phildh89@gmail.com>
2020-05-28 14:50:12 -0700

    Add project files.

---
+++ StarForceSimulation/Calculate.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarForceSimulation
+{
+    public class Calculate : Program
+    {
+        //Rate are % converted into int 95% = 9500
+        static int[] SuccessRate = { 9500, 9000, 8500, 8500, 8000, 7500, 7000, 6500, 6000, 5500, 5000, 4500, 4000, 3500, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 3000, 300, 200, 100 };
+        //Destroy Rates start at 12 going into 13
+        static int[] DestroyRate = { 60,130,140,210,210,210,280,280,700,700,1940,2940,3960};
+        static Random randomNum = new Random();
+
+        //No event or safeguard Simulation
+        public static void RunSimulation()
+        {
+            long countAttempts = 0;
+            long countDestroyed = 0;
+            long countSuccess = 0;
+            long countFail = 0;
+
+            for (int i = 0; i < numRepeat; i++)
+            {
+                for (int currentStar = BaseStars; currentStar < GoalStars;)
+                {
+                    //TODO: Visual stars method
+                    //TODO: Add Meso count
+                    int RNG = randomNum.Next(1, 10001);
+
+
+                    if (RNG <= SuccessRate[currentStar]) //Success
+                    {
+                        countSuccess++;
+                        currentStar++;
+                    }
+                    else if (RNG > SuccessRate[currentStar]) //Fail
+                    {
+                        countFail++;
+
+                        if (currentStar > 11) //Destroy
+                        {
+                            if (RNG >= 10000 - DestroyRate[currentStar - 12])
+                            {
+                                //Destroyed items go back to 12 and counts number of times destroyed
+                                currentStar = 12;
+                                countDestroyed++;
+                            }
+                            else
+                            {
+                                currentStar--;
+                            }
+                        }
+                        else if (currentStar > 10 && currentStar != 15 && currentStar != 20)
+                        {
+                            currentStar--;
+                        }
+
+                    }
+                    countAttempts++;
+                }
+            }
+            Console.WriteLine($"\n\tAverage outcome from {numRepeat} runs:");
+            Console.WriteLine($"\t\tAttempts: {countAttempts/numRepeat}");
+            Console.WriteLine($"\t\tSuccesses: {countSuccess/numRepeat}");
+            Console.WriteLine($"\t\tFailures: {countFail/ numRepeat}");
+            Console.WriteLine($"\t\tDestroyed items: {countDestroyed/numRepeat}");
+
+
+            Navigate();
+        }
+    }
+}
---
+++ StarForceSimulation/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Net;
+
+namespace StarForceSimulation
+{
+    public class Program
+    {
+        public static int EquipLevel = 300;
+        public static int MaxStars = 0;
+        public static int BaseStars = 0;
+        public static int GoalStars = 0;
+        public static int numRepeat = 0;
+        static void Main(string[] args)
+        {
+            WelcomeScreen();
+            Start();
+        }
+        public static void Start()
+        {
+            BaseInfo();
+            Calculate.RunSimulation();
+        }
+        public static void WelcomeScreen()
+        {
+            Console.WriteLine("\n\t-----------------------------------------");
+            Console.WriteLine("\t    Maple Story Star Force Simulation");
+            Console.WriteLine("\n\t      Press Any Key To Continue..");
+            Console.WriteLine("\t-----------------------------------------");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void BaseInfo()
+        {
+            //Selecting equipment level
+            do
+            {
+                try
+                {
+                    Console.Write("\n\tEnter the equipment level(0-275): ");
+                    EquipLevel = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Write("Invalid Entry");
+                }
+            } while (EquipLevel < 0 || EquipLevel > 275);
+
+            //Setting max star level
+            if (EquipLevel < 96)
+            {
+                MaxStars = 5;
+            }
+            else if (EquipLevel >95 && EquipLevel <108)
+            {
+                MaxStars = 8;
+            }
+            else if (EquipLevel > 107 && EquipLevel < 118)
+            {
+                MaxStars = 10;
+            }
+            else if (EquipLevel > 117 && EquipLevel < 128)
+            {
+                MaxStars = 15;
+            }
+            else if (EquipLevel > 127 && EquipLevel < 138)
+            {
+                MaxStars = 20;
+            }
+            else if (EquipLevel > 137)
+            {
+                MaxStars = 25;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+            //Selecting star base
+            do
+            {
+                try
+                {
+                    Console.Write($"\n\tEnter starting Star Force (0 - {MaxStars - 1}): ");
+                    BaseStars = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Write("Invalid Entry");
+                }
+            } while (BaseStars >= MaxStars || BaseStars < 0);
+
+            //Selecting star goal
+            do
+            {
+                try
+                {
+                    Console.Write($"\n\tEnter Final Star Force ({BaseStars + 1} - {MaxStars}): ");
+                    GoalStars = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Write("Invalid Entry");
+                }
+            } while (GoalStars < BaseStars && GoalStars > MaxStars);
+
+            //Number of times run
+            do
+            {
+                try
+                {
+                    Console.Write($"\n\tNumber of times you want to simulation to run: ");
+                    numRepeat = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Write("Invalid Entry");
+                }
+            } while (numRepeat < 0);
+
+        }
+        public static void Navigate()
+        {
+            char navigate = '0';
+
+            do
+            {
+                try
+                {
+                    Console.Write("\n\t\tEnter 1 to redo with same stats \n\t\t\tOR \n\t\tEnter 2 to start over..");
+                    navigate = Convert.ToChar(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Entry");
+                }
+            } while (navigate != '1' && navigate != '2');
+
+            if (navigate == '1')
+            {
+                Console.Clear();
+                Calculate.RunSimulation();
+            }
+            else
+            {
+                Console.Clear();
+                Start();
+            }
+
+        }
+    }
+}
